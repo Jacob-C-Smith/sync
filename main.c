a0faf36afeb60fbee0ee0b421d12911aa5748f5f
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-03-19 17:23:08 -0600

    [example] Updated example program

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 // sync 
 #include <sync/sync.h>
@@ -17,7 +18,37 @@
 // Preprocessor definitions
 #define NTH_FIBONACCI_NUMBER 1000000000
 
+// Enumeration definitions
+enum sync_examples_e
+{
+    SYNC_TIMER_EXAMPLE     = 0,
+    SYNC_MUTEX_EXAMPLE     = 1,
+    SYNC_SEMAPHORE_EXAMPLE = 2,
+    SYNC_MONITOR_EXAMPLE   = 3,
+    SYNC_EXAMPLE_QUANTITY  = 4
+};
+
 // Forward declarations
+/** !
+ * Print a usage message to standard out
+ * 
+ * @param argv0 the name of the program
+ * 
+ * @return void
+ */
+void print_usage ( const char *argv0 );
+
+/** !
+ * Parse command line arguments
+ * 
+ * @param argc            the argc parameter of the entry point
+ * @param argv            the argv parameter of the entry point
+ * @param examples_to_run return
+ * 
+ * @return void on success, program abort on failure
+ */
+void parse_command_line_arguments ( int argc, const char *argv[], bool *examples_to_run );
+
 /** !
  * Timer example program
  * 
@@ -67,30 +98,59 @@ int main ( int argc, const char *argv[] )
     (void) argv;
 
     // Initialized data
-    mutex m  = { 0 };
+    bool examples_to_run[SYNC_EXAMPLE_QUANTITY] = { 0 };
 
-    // Run the timer example program
-    if ( sync_timer_example(argc, argv) == 0 ) goto failed_to_run_sync_example;
+    // Parse command line arguments
+    parse_command_line_arguments(argc, argv, examples_to_run);
 
-    // Run the mutex example program
-    if ( sync_mutex_example(argc, argv) == 0 ) goto failed_to_run_mutex_example;
+    // Formatting
+    printf(
+        "╭──────────────╮\n"\
+        "│ sync example │\n"\
+        "╰──────────────╯\n"\
+        "The sync library provides cross platform synchronization primitives and high-precision timing\n"\
+        "Sync provides %d abstractions. The timer, the mutex, the semaphore, and the monitor.\n\n"\
+        "The timer is used for collecting high percision timestsamps.\n"\
+        "A mutex is the most basic synchronization primitive. It provides mutual exclusion to code.\n"\
+        "A semaphore is an abstraction for controlling access to a resource.\n"\
+        "A monitor is an abstraction for blocking and signaling threads in a critical section.\n\n",
+        SYNC_EXAMPLE_QUANTITY
+    );
 
-    // Run the semaphore example program
-    #ifdef BUILD_SYNC_WITH_SEMAPHORE
-        if ( sync_semaphore_example(argc, argv) == 0 ) goto failed_to_run_semaphore_example;
-    #endif
+    //////////////////////
+    // Run the examples //
+    //////////////////////
 
-    // Run the monitor example program
-    #ifdef BUILD_SYNC_WITH_MONITOR
-        if ( sync_monitor_example(argc, argv) == 0 ) goto failed_to_run_monitor_example;
-    #endif
+    // Run the thread example program
+    if ( examples_to_run[SYNC_TIMER_EXAMPLE] )
+
+        // Error check
+        if ( sync_timer_example(argc, argv) == 0 ) goto failed_to_run_timer_example;
     
+    // Run the mutex example program
+    if ( examples_to_run[SYNC_MUTEX_EXAMPLE] )
+
+        // Error check
+        if ( sync_mutex_example(argc, argv) == 0 ) goto failed_to_run_mutex_example;
+    
+    // Run the semaphore example program
+    if ( examples_to_run[SYNC_SEMAPHORE_EXAMPLE] )
+
+        // Error check
+        if ( sync_semaphore_example(argc, argv) == 0 ) goto failed_to_run_semaphore_example;
+    
+    // Run the monitor example program
+    if ( examples_to_run[SYNC_MONITOR_EXAMPLE] )
+
+        // Error check
+        if ( sync_monitor_example(argc, argv) == 0 ) goto failed_to_run_monitor_example;
+
     // Success
     return EXIT_SUCCESS;
 
     // Error handling
     {
-        failed_to_run_sync_example:
+        failed_to_run_timer_example:
 
             // Write an error message to standard out
             printf("Failed to run timer example!\n");
@@ -123,6 +183,95 @@ int main ( int argc, const char *argv[] )
             return EXIT_FAILURE;
     }
 }
+
+
+void print_usage ( const char *argv0 )
+{
+
+    // Argument check
+    if ( argv0 == (void *) 0 ) exit(EXIT_FAILURE);
+
+    // Print a usage message to standard out
+    printf("Usage: %s [timer] [mutex] [semaphore] [monitor]\n", argv0);
+
+    // Done
+    return;
+}
+
+void parse_command_line_arguments ( int argc, const char *argv[], bool *examples_to_run )
+{
+
+    // If no command line arguments are supplied, run all the examples
+    if ( argc == 1 ) goto all_examples;
+
+    // Error check
+    if ( argc > SYNC_EXAMPLE_QUANTITY + 1 ) goto invalid_arguments;
+
+    // Iterate through each command line argument
+    for (size_t i = 1; i < argc; i++)
+    {
+        
+        // Timer example?
+        if ( strcmp(argv[i], "timer") == 0 )
+
+            // Set the thread example flag
+            examples_to_run[SYNC_TIMER_EXAMPLE] = true;
+
+        // Mutex example?
+        else if ( strcmp(argv[i], "mutex") == 0 )
+            
+            // Set the thread pool example flag
+            examples_to_run[SYNC_MUTEX_EXAMPLE] = true;
+
+        // Semaphore example?
+        else if ( strcmp(argv[i], "semaphore") == 0 )
+
+            // Set the schedule example flag
+            examples_to_run[SYNC_SEMAPHORE_EXAMPLE] = true;
+        
+        // Monitor example?
+        else if ( strcmp(argv[i], "monitor") == 0 )
+
+            // Set the schedule example flag
+            examples_to_run[SYNC_MONITOR_EXAMPLE] = true;
+
+        // Default
+        else goto invalid_arguments;
+    }
+    
+    // Success
+    return;
+
+    // Set each example flag
+    all_examples:
+    {
+
+        // For each example ...
+        for (size_t i = 0; i < SYNC_EXAMPLE_QUANTITY; i++)
+        
+            // ... set the example flag
+            examples_to_run[i] = true;
+        
+        // Success
+        return;
+    }
+
+    // Error handling
+    {
+
+        // Argument errors
+        {
+            invalid_arguments:
+                
+                // Print a usage message to standard out
+                print_usage(argv[0]);
+
+                // Abort
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
 
 int sync_timer_example ( int argc, const char *argv[] )
 {
